711ca72f3281a63cdac59e00080b7b67d9e93eed
anna-mish <50827082+anna-mish@users.noreply.github.com>
2019-09-05 12:51:33 +0300

    Add files via upload

---
+++ Adaptive menu jQuery/script.js
@@ -0,0 +1,13 @@
+ $(document).ready(function(){
+
+   var link = $('#hamburger');
+  //  var link_active = $('.menu-link_active');
+   var menu = $('#mobile-navigation');
+  //  var nav_link = $('.menu a')
+
+   link.click(function(){
+       link.toggleClass('menu-link_active');
+       menu.toggleClass('menu_active');
+   });
+ 
+ }); 
